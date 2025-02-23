3b23a6dba099140a5d159ebc60b6148ca86682f1
pablocasamianestudios <pablocasamianestudios@gmail.com>
2025-02-23 18:32:18 +0100

    mejorando estructura

--- hundirFlota.js
+++ hundirFlota.js
@@ -2,13 +2,15 @@ window.onload = function() {
     const tablero = document.getElementById("tablero");
     const tamanio = 10;
 
-    function crearTablero() {
-        for (let i = 0; i < tamanio * tamanio; i++) {
-            const celda = document.createElement("div");
-            celda.classList.add("celda");
-            celda.dataset.indice = i;
-            tablero.appendChild(celda);
-        }
-    }
+  
     crearTablero();
+}
+
+function crearTablero() {
+    for (let i = 0; i < tamanio * tamanio; i++) {
+        const celda = document.createElement("div");
+        celda.classList.add("celda");
+        celda.dataset.indice = i;
+        tablero.appendChild(celda);
+    }
 }
